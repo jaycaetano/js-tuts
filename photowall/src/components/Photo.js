e177d448608b05acc9958d5b787e7254453325d9
Jay Caetano <jasonlcaetano@gmail.com>
2019-11-10 21:06:28 -0500

    more updates

--- photowall/src/components/Photo.js
+++ photowall/src/components/Photo.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Photo extends Component {
-    render() {
-        const post = this.props.post
+function Photo (props) {
+    const post = props.post
         return <figure className='figure'>
                 <img className='photo' src={post.imageLink} alt={post.descritpion}/>
                 <figcaption><p>{post.descritpion}</p></figcaption>
                 <div className ='button-contianer'>
-                    <button className= 'remove-button'>Remove</button>
+                    <button className= 'remove-button' onClick= {() => {
+                        props.onRemovePhoto(post)
+                    }}>Remove</button>
                 </div>
         </figure>
-    }
 }
+
 
 export default Photo
--- photowall/src/components/Title.js
+++ photowall/src/components/Title.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Title extends Component {
-    render () {
+function Title (props){
     return (
-        <h1>{this.props.title}</h1>
+        <h1>{props.title}</h1>
     )
-    }
 }
 
 export default Title
