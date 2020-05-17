7785bfb73cb7bbe86213c4721e8602583d428fd0
Jay Caetano <jasonlcaetano@gmail.com>
2020-05-17 16:32:35 -0400

    remove photowall

--- photowall/src/index.js
+++
@@ -1,6 +0,0 @@
- import React from 'react';
- import ReactDOM from 'react-dom';
- import Main from './components/Main';
- import './styles/stylesheet.css'
-
- ReactDOM.render(<Main/>, document.getElementById('root'));
